bd24289c10749969e58907ad999be0c4ef433837
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-23 05:52:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  my_array = array[-1]
+  p my_array
 end
 
 def update_element_from_index(array, index_number, element)
